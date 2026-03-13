53b6f16186a13b8a4eee01b56c26ac56d5d734c0
yosvelastoponce-bit <yosvelastoponce@gmail.com>
2026-03-13 14:16:40 -0500

    der

--- resources/js/Pages/App/Index.vue
+++ resources/js/Pages/App/Index.vue
@@ -1,6 +1,10 @@
 <script setup>
-
+// Aquí irá tu lógica de JavaScript
 </script>
+
 <template>
-    
+    <div class="p-6">
+        <h1 class="text-2xl font-bold">Página de Inicio de App</h1>
+        <p>Este es el archivo Index.vue que estaba vacío.</p>
+    </div>
 </template>
